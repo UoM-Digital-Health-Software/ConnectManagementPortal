e2ad55f69e5744c4eab4f0dd6dbb4c9b001efa85
Gwen Zhou <jiawen.zhou@manchester.ac.uk>
2025-09-01 16:58:35 +0100

    change default version

--- src/main/java/org/radarbase/management/web/rest/VersionResource.kt
+++ src/main/java/org/radarbase/management/web/rest/VersionResource.kt
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/versions")
 class VersionRescource(
     private val managementPortalProperties: ManagementPortalProperties
 ) {
-    @GetMapping("/min-required-version")
+    @GetMapping("min-required-version")
     fun getMinRequiredVersion(): Map<String, String> {
         val minVersion = managementPortalProperties.getMinRequiredVersion()
         return mapOf(
